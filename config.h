75c521b672da5417e57ef111cdd3c601fcf468f2
jonay2000 <jonabent@gmail.com>
2019-05-08 23:27:26 +0200

    changed color scheme

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -81,48 +81,45 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-float alpha = 0.8;
+float alpha = 0.5;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#231f20", /* black   */
+  [1] = "#ee2e24", /* red     */
+  [2] = "#00853e", /* green   */
+  [3] = "#ffd204", /* yellow  */
+  [4] = "#009ddc", /* blue    */
+  [5] = "#98005d", /* magenta */
+  [6] = "#85cebc", /* cyan    */
+  [7] = "#d9d8d8", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#737171", /* black   */
+  [9]  = "#ee2e24", /* red     */
+  [10] = "#00853e", /* green   */
+  [11] = "#ffd204", /* yellow  */
+  [12] = "#009ddc", /* blue    */
+  [13] = "#98005d", /* magenta */
+  [14] = "#85cebc", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+  /* special colors */
+  [256] = "black", /* background */
+  [257] = "#d9d8d8", /* foreground */
 };
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor, reversecursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
-
 /*
  * Default shape of cursor
  * 2: Block ("█")
