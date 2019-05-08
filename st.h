f47b1532f5e55551afb4a3b54f9267e6edc0b837
jonay2000 <jonabent@gmail.com>
2019-05-08 23:05:25 +0200

    added iso

--- st.h
+++ st.h
@@ -89,6 +89,7 @@ void draw(void);
 
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
+void iso14755(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
